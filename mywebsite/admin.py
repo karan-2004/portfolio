a7c9bd9cd36fe4de127a4860be15df3739d16253
karan <kara80499@gmail.com>
2023-12-22 12:44:38 +0530

    model created and added in adminsite

--- mywebsite/admin.py
+++ mywebsite/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.db.models import get_models, get_app
+from django.contrib.admin.sites import AlreadyRegistered
+
+#Any number of arguments can be passed on to the auto_register which 
+#will be taken as a list with the help of *args
+#get_app gets the app object with the help of app name provided
+#get_models gets the list of models in the specific app
+def auto_register(*apps):
+    for app_name in apps:
+        app = get_app(app_name)
+        for model in get_models(app):
+            try:
+                admin.site.register(model)
+            except AlreadyRegistered:
+                pass
+
+auto_register('mywebsite')
+
 
 # Register your models here.
--- mywebsite/models.py
+++ mywebsite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class certificates(models.Model):
+    certificates_images = models.ImageField(upload_to='certificates')
+    description = models.TextField()
+
+
+    
